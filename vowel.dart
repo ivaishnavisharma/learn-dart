63a354c282fc73fc4dc993fcbd9c7bf80b2c3842
ivaishnavisharma <satyamsharma1905@gmail.com>
2023-07-05 12:52:58 +0530

    new file added

---
+++ vowel.dart
@@ -0,0 +1,17 @@
+void main(){
+ String value='b';
+if(value == 'a'){
+    print ('vowel');
+}else if(value=='e'){
+    print('vowel');
+}else if(value=='i'){
+    print('vowel');
+}else if(value=='o'){
+    print('vowel');
+}else if(value=='u'){
+    print('vowel');
+}else {
+    print('constant');
+}
+
+}
